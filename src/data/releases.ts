ce4cf7ea6ab200023f822a840d8502b7736d1be2
radimkocourek94 <radim.kocourek94@gmail.com>
2026-03-14 16:37:22 +0100

    feat: add Bandcamp album embed for "Collections of Pink, Blue, and All the Other Colors, Too" and improve layout on music page

--- src/data/releases.ts
+++ src/data/releases.ts
@@ -11,7 +11,8 @@ export const releases: Release[] = [
     title: "Collections of Pink, Blue, and All the Other Colors, Too",
     year: 2025,
     coverArt: "/images/collections-cover.jpg",
-    albumEmbedHtml: "", // paste Bandcamp album embed here
+    albumEmbedHtml: `<!-- Bandcamp album embed (fixed size) -->
+<iframe style="border: 0; width: 700px; height: 472px;" src="https://bandcamp.com/EmbeddedPlayer/album=408392112/size=large/bgcol=333333/linkcol=ffffff/artwork=small/transparent=true/" seamless><a href="https://lishkee.bandcamp.com/album/collections-of-pink-blue-all-the-other-colors-too">Collections of Pink, Blue, &amp; All the Other Colors, Too by Lishkee</a></iframe>`,
     description: "A combination of dream pop and dark electronica.",
   },
 ]
